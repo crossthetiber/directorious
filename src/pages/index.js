bc388e6ebbc8983057a511339a9ce9276a8562cf
Michael Angelus <robertjonfox@gmail.com>
2019-04-09 16:09:46 -0400

    Start customizing index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Staff Directory</h1>
+    <p>This is the library staff directory</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
